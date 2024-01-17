740c92522b03103830f80c14626e5fd02b0cd1b2
AdarshKumar12345 <adarsh8507kumar@gmail.com>
2024-01-17 08:45:59 +0530

    day 1

--- snake_game.py
+++ snake_game.py
@@ -1,1 +1,20 @@
-print( "hello world ")
+import pygame,sys
+
+pygame.init()
+screen = pygame.display.set_mode((400,500))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    pygame.display.update()
+    clock.tick(60)
+    
+    
+
+
+
+
