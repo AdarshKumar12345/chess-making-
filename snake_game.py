1644ec5cf481dc58d50a1bae4de01eb81a9695d5
AdarshKumar12345 <adarsh8507kumar@gmail.com>
2024-01-17 18:48:22 +0530

    day2

--- snake_game.py
+++ snake_game.py
@@ -1,8 +1,42 @@
-import pygame,sys
+import pygame,sys , random
+
+from pygame.math import Vector2
+
+class Snake :
+    def __init__(self) :
+        self.body = [ Vector2(5,10),Vector2(6,10),Vector2(7,10)]
+    
+    def draw_snake(self) :
+        for block in self.body :
+            x_pose = int (block.x *cell_size )
+            y_pose = int (block.y * cell_size )
+            
+            block_rect = pygame.Rect(x_pose ,y_pose ,cell_size ,cell_size )
+            pygame.draw.rect(screen ,(183,192,122), block_rect )
+            
+                        
+        
+
+class Fruit :
+    def __init__(self) :
+        self.x = random.randint( 0 ,cell_number -1)
+        self.y =random.randint( 0 , cell_number -1)
+        
+        self.pos = Vector2(self.x, self.y )
+        
+    def draw_fruit(self)  :
+        fruit_rect =pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y *cell_size), cell_size , cell_size )
+        pygame.draw.rect(screen ,(126,166,114),fruit_rect)
 
 pygame.init()
-screen = pygame.display.set_mode((400,500))
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number *cell_size , cell_number * cell_size) )
 clock = pygame.time.Clock()
+fruit = Fruit()
+snake =Snake()
+
+
 
 while True:
     for event in pygame.event.get():
@@ -10,6 +44,11 @@ while True:
             pygame.quit()
             sys.exit()
             
+    screen.fill((175,215,70))
+    fruit.draw_fruit()
+    snake.draw_snake()
+    
+    
     pygame.display.update()
     clock.tick(60)
     
